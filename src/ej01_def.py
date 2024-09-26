10fce9e37a9ecf68f2c62b379017f0b71a4b9300
dbenfon341 <dbenfon341@g.educaand.es>
2024-09-26 20:16:06 +0200

    Edición de comentarios.

--- src/ej01_def.py
+++ src/ej01_def.py
@@ -1,4 +1,4 @@
-# Ejercicio 1.2.1¶
+# Ejercicio 1.2.1
 # Escribe un programa que pida el nombre del usuario para luego darle la bienvenida.
 #
 # Escribe tu nombre: Juan
--- src/ej03.py
+++ src/ej03.py
@@ -1,4 +1,4 @@
-# Ejercicio 1.2.3¶
+# Ejercicio 1.2.3
 # Suponiendo que se han ejecutado las siguientes sentencias de asignación:
 
 # ancho = 17
