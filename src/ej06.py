205d0f00ed44c97271a860e8b17eb5a1d3f4eab1
dbenfon341 <dbenfon341@g.educaand.es>
2024-09-30 18:37:30 +0200

    Comienzo ej06

--- src/ej06.py
+++ src/ej06.py
@@ -1,10 +1,15 @@
 # Ejercicio 1.2.6
 # Escribe un programa que pida el importe final de un artículo y calcule e imprima por pantalla el IVA que se ha pagado y el importe sin IVA (suponiendo que se ha aplicado un tipo de IVA del 10%).
 
+def calcular_importe(precio_articulo: int, iva) -> tuple:
 
+    importe_iva: float
+    importe_no_iva: float
+    return
 
 
 def main():
+    IVA_10 = 10
     test = test
 
 if __name__ == "__main__":
