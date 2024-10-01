b8320d3be2a611c8b406a676fae33b81dcae2397
dbenfon341 <dbenfon341@g.educaand.es>
2024-10-01 17:27:46 +0200

    Cambios half half

--- src/ej11.py
+++ src/ej11.py
@@ -3,12 +3,18 @@
 # desde 1 hasta n. La suma de los n primeros enteros positivos puede ser calculada de la siguiente forma:
 # suma = n(n+1)/2
 
-def main():
-    n: int = int(input("Introduce un número: "))
+def contar_numeros(contar_num: int) -> int:
     numero_completo: int = 0
-    for i in range(0, n+1):
+    for i in range(0, contar_num+1):
         numero_completo += i
-    print(numero_completo)
+    return numero_completo
+
+def main():
+    try:
+        contar_num: int = int(input("Introduce un número: "))
+        print(f"La suma de los números enteros es: {contar_numeros(contar_num)}")
+    except ValueError as e:
+        print(f"**ERROR** Solo puedo tratar con números. Código de error -> {e}")
 
 if __name__ == "__main__":
     main()
--- src/ej30.py
+++ src/ej30.py
@@ -5,8 +5,31 @@
 # que introduzcan un valor correcto para ambos (os lo dejo a vuestra elección, la primera opción es más fácil, aunque el mundo está lleno de valientes).
 # Por ejemplo, si introducen los valores 1, 1 y 10, el programa mostrará en consola exactamente lo siguiente: SERIE => 1-2..3..4..5..6..7..8..9-10
 
+def serie(inicio: float, incremento: int, total: int) -> str:
+    serie = 0
+    serie_string: str = ""
+    for i in range (inicio, total, incremento):
+        serie += i
+        serie_string = serie + "-"
+    serie =+ "-" + total
+
+    return serie
+
 def main():
-    test = test
+    validar_incremento = True
+    validar_total = True
+    try:
+        while validar_incremento:
+            incremento: int = int(input("Dame un incremento: "))
+            if incremento < 0:
+                print("El número debe ser mayor que 0.")
+            else:
+                validar_incremento: False
+
+        print(f"{serie(inicio, incremento, total)}")
+
+    except ValueError as e:
+        print(f"**ERROR** Valores incorrectos. Código de error -> {e}")
 
 if __name__ == "__main__":
     main()
