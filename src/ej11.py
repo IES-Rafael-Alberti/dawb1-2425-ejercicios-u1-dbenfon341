00126b77531823d978f53a5de646d70eb562fe0c
dbenfon341 <dbenfon341@g.educaand.es>
2024-10-03 16:54:13 +0200

    Modified ej11 with doc.

--- src/ej11.py
+++ src/ej11.py
@@ -4,6 +4,12 @@
 # suma = n(n+1)/2
 
 def contar_numeros(contar_num: int) -> int:
+    """ Funcion para contar desde 0 hasta input introducido por el usuario para luego sumar y actualizar una variable llamada numero_completo con la suma de todos ellos.
+    Args:
+        contar_numeros(int): La función sumará números hasta el valor de este parámetro.
+    Returns:
+        numero_completo(int): Retornará una variable con el valor de la suma de todos los numeros desde 0 hasta contar_num.
+    """
     numero_completo: int = 0
     for i in range(0, contar_num+1):
         numero_completo += i
