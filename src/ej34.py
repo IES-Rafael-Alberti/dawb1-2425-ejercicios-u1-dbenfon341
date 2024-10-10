17878b1d3bed7a597038b180f92a922d40ce2741
dbenfon341 <dbenfon341@g.educaand.es>
2024-10-10 21:08:00 +0200

    Cinco nuevos ejercicios.

---
+++ src/ej34.py
@@ -0,0 +1,7 @@
+# Mostrar todos los divisores de un número
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/ej35.py
@@ -0,0 +1,7 @@
+# Calcular la serie de Fibonacci hasta un número dado
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
