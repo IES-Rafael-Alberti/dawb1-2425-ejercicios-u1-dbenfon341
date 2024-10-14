1023c16f51105c51df88b51475cacaa39172a91e
dbenfon341 <dbenfon341@g.educaand.es>
2024-10-14 18:47:03 +0200

    Terminado sin documentacion.

--- src/adivina_numero.py
+++ src/adivina_numero.py
@@ -1,40 +1,189 @@
 # Adivina el número utilizando randomint y un contador en bucle while.
 
 import random
+from utils import clear
 
-def obtener_numero(num1: int, num2: int):
-    return
+##########################################################################################################################
+# Variables con valores por defecto para adivinar el número.
+# num_base: se utiliza como el numero base que se utilizará en el rango desde el que se genera el número aleatorio.
+# num_limite: se utiliza como el numero límite que se utilizara en el rango desde el que se genera el número aleatorio.
+#
+# Ejemplo: "num_base 1 + num_limite 100" generará un número al azar entre 1 y 100 (1-100).
+# intentos: número máximo de intentos que tiene el usuario para adivinar el número.
+##########################################################################################################################
 
-def validar_numero():
-    return
+num_base = 1
+num_limite = 100
+intentos = 10
 
-def numero_caotico():
-    return
+##########################################################################################################################
+# Función para configurar las variables por defecto (si el usuario quiere)
+##########################################################################################################################
 
-def main():
-    numero_oculto = random.randint(1, 10)
-    contador = 10
+def configurar_numero_oculto() -> tuple:
+    """
+    Función que sirve para configurar el juego.
+    Valida si el input del usuario es correcto y devuelve la configuración deseada por el usuario.
 
-    print(f"Introduce un número desde 1 hasta 500. Tienes {contador} intentos.")
+    Returns:
+    tuple: Una tupla con las tres configuraciones dadas.
+        - num_base(int): El número mínimo.
+        - num_limite(int): El número máximo.
+        - intentos(int): El número de intentos.
+    """
+    global num_base, num_limite, intentos
+    
+    num_base = None
+    num_limite = None
+    intentos = None
 
-    while contador > 0:
-        numero_usuario = int(input("Introduce tu número: "))
+    while num_base is None:
+        try:
+            num_base = int(input("Introduce el primer número (mayor que 0): "))
+            if num_base <= 0:
+                print("**ERROR** El número debe ser mayor que 0.")
+                num_base = None
+        except ValueError:
+            print("No has introducido un número. Vuelve a intentarlo: ")
 
+    while num_limite is None:
+        try:
+            num_limite = int(input("Introduce el numero máximo (mayor que 0): "))
+            if num_limite <= 0:
+                print("**ERROR** El número debe ser mayor que 0.")
+                num_limite = None
+        except ValueError:
+            print("No has introducido un número. Vuelve a intentarlo: ")
+
+    while intentos is None:
+        try:
+            intentos = int(input("Introduce la cantidad de intentos: "))
+            if intentos <= 0:
+                print("**ERROR** El número debe ser mayor que 0.")
+                intentos = None
+        except ValueError:
+            print("No has introducido un número. Vuelve a intentarlo: ")
+
+    return num_base, num_limite, intentos
+
+
+def menu():
+    """
+    
+    """
+    clear()
+    print("********************")
+    print("* ADIVINA EL JUEGO *")
+    print("********************")
+    print("1) Jugar.")
+    print("2) Configuracion.")
+    print("3) Salir.")
+
+
+def seleccionar_opcion(opcion: int):
+    """
+    
+    """
+    if opcion == 1:
+        start()
+    elif opcion == 2:
+        configurar_numero_oculto()
+    elif opcion == 3:
+        print("Hasta pronto!")
+        exit
+
+
+def introducir_opcion() -> int:
+    """
+    
+    """
+    menu()
+    OPCIONES = (1, 2, 3)
+    opcion = None
+    while opcion is None:
+        try:
+            opcion = int(input("Introduce opción (1-3): "))
+            if opcion not in OPCIONES:
+                print(f"**ERROR** La opción {opcion} no existe.")
+                opcion = None
+        except ValueError:
+            print("No has introducido un número")
+    return opcion
+
+def validar_numero_usuario() -> int:
+    """
+    
+    """
+    numero_usuario = None
+    while numero_usuario is None:
+        try:
+            numero_usuario = int(input("Introduce tu número: "))
+        except ValueError:
+            print("Debes introducir un número.")
+    return numero_usuario
+
+
+def generar_numero_oculto(num_base: int, num_limite: int):
+    """"""
+    return random.randint(num_base, num_limite)
+
+
+def start():
+    """
+    
+    """
+    global num_base, num_limite, intentos
+
+    numero_oculto = generar_numero_oculto(num_base, num_limite)
+
+    clear()
+    print(f"Introduce un número desde {num_base} hasta {num_limite}. Tienes {intentos} intentos.")
+
+    while intentos > 0:
+        try:
+            numero_usuario = validar_numero_usuario()
+        except ValueError:
+            print("Debes introducir un número.")
         if numero_usuario > numero_oculto:
-            contador -= 1
-            print(f"El número debe ser más bajo. Te quedan {contador} intentos.")
+            intentos -= 1
+            clear()
+            print(f"Tu número es: {numero_usuario}.")
+            print(f"El número oculto está entre {num_base} y {num_limite}.")
+            print(f"Tu número debe ser más bajo. Te quedan {intentos} intentos.")
         elif numero_usuario < numero_oculto:
-            contador -= 1
-            print(f"El número debe ser más alto. Te quedan {contador} intentos.")
+            clear()
+            intentos -= 1
+            print(f"Tu número es: {numero_usuario}.")
+            print(f"El número oculto está entre {num_base} y {num_limite}.")
+            print(f"El número debe ser más alto. Te quedan {intentos} intentos.")
         else:
-            print("Ganaste! ¿jugar de nuevo?")
+            clear()
+            print(f"Tu número es: {numero_usuario}.")
+            print(f"¡Ganaste! el número oculto era {numero_oculto} y acabaste con {intentos} intento/s restantes. ¿jugar de nuevo?")
             repetir_juego = input("S/N").upper()
             if repetir_juego == "S":
-                contador = 10
-                numero_oculto = random.randint(1, 10)
+                clear()
+                seleccionar_opcion(introducir_opcion())
+                numero_oculto = generar_numero_oculto(num_base, num_limite)
+            else:
+                print("Hasta pronto!")
     else:
-        print(f"Vaya, no adivinaste el número. El número oculto era {numero_oculto}.")
+        clear()
+        print(f"Tu número es: {numero_usuario}")
+        print(f"Vaya, no adivinaste el número. El número oculto era {numero_oculto}. ¿Jugar de nuevo?")
+        repetir_juego = input("S/N").upper()
+        if repetir_juego == "S":
+                clear()
+                seleccionar_opcion(introducir_opcion())
+                numero_oculto = generar_numero_oculto(num_base, num_limite)
 
+
+def main():
+    """
+    
+    """
+    while True:
+        seleccionar_opcion(introducir_opcion())
 
 if __name__ == "__main__":
     main()
